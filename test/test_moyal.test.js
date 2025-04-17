808b54a3c3f3468beea4517c508b86ac8873b9b9
Ilan Moyal <ilan.amoyal@gmail.com>
2025-04-17 18:38:32 +0300

    version 1.0.0 - still under working

---
+++ test/test_moyal.test.js
@@ -0,0 +1,6 @@
+import "../src/moyal.test.js";
+
+// let testGroup = new moyal.test.TestGroup("Test moyal.test using moyal.test")
+// .areEqual("test areEquals", true, true).run(true);
+
+moyal.test.areEqual("dummy", 4, 3 + 1);
